a244c5c5499e4d48202eb3a08a347af0c0566437
Ilatyphi95 <ilatyphi95@gmail.com>
2019-04-15 14:46:52 +0100

    spinner layout created, create reference to spinner in main activity

--- app/src/main/java/com/ilatyphi95/populatespinnerwithsqlitedatabase/MainActivity.java
+++ app/src/main/java/com/ilatyphi95/populatespinnerwithsqlitedatabase/MainActivity.java
@@ -2,6 +2,7 @@ package com.ilatyphi95.populatespinnerwithsqlitedatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spnFoodList = findViewById(R.id.spn_food_list);
+
     }
 }
